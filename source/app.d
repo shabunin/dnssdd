d62c37a192900880ec2b969bfc961193b1f65cad
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2019-08-06 23:41:44 +0300

    work in progress

--- source/app.d
+++ source/app.d
@@ -4,6 +4,7 @@ import std.bitmanip;
 import core.sys.posix.netinet.in_;
 import core.sys.linux.netinet.in_ : IP_ADD_MEMBERSHIP, IP_MULTICAST_LOOP;
 
+import record_classes_types;
 
 void main()
 {
@@ -41,17 +42,11 @@ void main()
     if(receivedLen > 0)
     {
       buf.length = receivedLen;
-      writeln("is data, len: ", receivedLen);
 
       // TODO: comments and docs
 
       // parse labels
-      struct Label {
-        bool valid;
-        ushort length;
-        char[] domain_name;
-      }
-      Label _parseLabel(ubyte[] buf, int offset) {
+      RecordLabel _parseLabel(ubyte[] buf, int offset) {
         // TODO: reverse engineering
         char[][] labels;
         // length of array
@@ -60,10 +55,10 @@ void main()
         while(true) {
           auto label_len = buf.peek!ubyte(offset + length);
           if ((label_len & 0b11000000) == 0b11000000) {
-            // compression. rfc1035 4.1.4
+            // compression rfc1035 4.1.4
             auto i = buf.peek!ushort(offset + length) & 0b0011111111111111;
             auto parsed = _parseLabel(buf, i);
-            if (parsed.length > 0) {
+            if (parsed.valid) {
               labels.length += 1;
               labels[$ - 1] = parsed.domain_name;
               length += 2;
@@ -100,12 +95,29 @@ void main()
           domain_name ~= labels[j];
         }
 
-        Label result;
+        RecordLabel result;
         result.valid = valid;
         result.length = length;
         result.domain_name = domain_name;
 
         return result;
+      }
+
+      void _parseRdataSrv(ubyte[] buf, int offset, int len) {
+        if (len <= 6) {
+          return;
+        }
+        string target = "".dup;
+        RecordLabel parsed = _parseLabel(buf, offset + 6);
+        writeln(parsed);
+        if (parsed.valid) {
+          target = cast(string) parsed.domain_name;
+        }
+        ushort priority = buf.peek!ushort(offset);
+        ushort weight = buf.peek!ushort(offset + 2);
+        ushort port = buf.peek!ushort(offset + 4);
+        writeln("_parseRdataSrv:: ", target);
+        writeln("priority: ", priority, ", weight: ", weight, " port: ", port);
       }
 
       // parse general
@@ -192,20 +204,40 @@ void main()
             if (offset + 4 > buf.length) {
               break;
             }
-            auto type_value = buf.peek!ushort(offset);
+            string record_type = "".dup;
+            string record_class = "".dup;
+            ushort type_value = buf.peek!ushort(offset);
+            writeln("type_value: ", type_value);
+            if ((type_value in RecordTypes) !is null) {
+              record_type = RecordTypes[type_value];
+            }
+            writeln("type: ", record_type);
             offset += 2;
-            auto csl_value = buf.peek!ushort(offset);
+            ushort cls_value = buf.peek!ushort(offset);
+            writeln("cls_value: ", cls_value);
+            if ((cls_value in RecordClasses) !is null) {
+              record_class = RecordClasses[cls_value];
+            }
             offset += 2;
             writeln("questin for: ", parsed.domain_name);
-            writeln("type: ", type_value, " csl: ", csl_value);
+            writeln("type: ", record_type, " class: ", record_class);
           } else {
+            string record_type = "".dup;
+            string record_class = "".dup;
             if (offset + 10 > buf.length) {
               break;
             }
-            auto type_value = buf.peek!ushort(offset);
+            ushort type_value = buf.peek!ushort(offset);
+            if ((type_value in RecordTypes) !is null) {
+              record_type = RecordTypes[type_value];
+            }
             offset += 2;
-            auto csl_value = buf.peek!ushort(offset);
-            auto flash = csl_value & 0b1000000000000000;
+            ushort cls_value = buf.peek!ushort(offset);
+            ushort cls_key = cls_value & 0b0111111111111111;
+            if ((cls_key in RecordClasses) !is null) {
+              record_class = RecordClasses[cls_key];
+            }
+            ushort flash = cls_value & 0b1000000000000000;
             offset += 2;
             auto ttl = buf.peek!uint(offset);
             offset += 4;
@@ -215,8 +247,11 @@ void main()
               break;
             }
             offset += rdlen;
+            if (record_type == "SRV") {
+              _parseRdataSrv(buf, offset, rdlen);
+            }
             writeln("answer for: ", parsed.domain_name);
-            writeln("type: ", type_value, " csl: ", csl_value);
+            writeln("type: ", record_type, " class: ", record_class, " flash: ", flash);
             writeln("ttl: ", ttl, " rdlen: ", rdlen);
           }
 
---
+++ source/record_classes_types.d
@@ -0,0 +1,78 @@
+module record_classes_types;
+
+import std.stdio;
+import std.exception : assumeUnique;
+
+struct RecordLabel {
+  bool valid;
+  ushort length;
+  char[] domain_name;
+}
+
+immutable string[ushort] RecordClasses; 
+immutable string[ushort] RecordTypes;
+
+static this() {
+  string[ushort] t = [
+    1: "INT",
+    2: "CS",
+    3: "CH",
+    4: "HS"
+  ];
+  RecordClasses = assumeUnique(t);
+  t = [
+    0: "UNKNOWN",
+    1: "A",
+    2: "NS",
+    3: "MD",
+    4: "MF",
+    5: "CNAME",
+    6: "SOA",
+    7: "MB",
+    8: "MG",
+    9: "MR",
+    10: "NULL",
+    11: "WKS",
+    12: "PTR",
+    13: "HINFO",
+    14: "MINFO",
+    15: "MX",
+    16: "TXT",
+    28: "AAAA",
+    18: "AFSDB",
+    42: "APL",
+    257: "CAA",
+    60: "CDNSKEY",
+    59: "CDS",
+    37: "CERT",
+    49: "DHCID",
+    32769: "DLV",
+    39: "DNAME",
+    48: "DNSKEY",
+    43: "DS",
+    55: "HIP",
+    45: "IPSECKEY",
+    25: "KEY",
+    36: "KX",
+    29: "LOC",
+    35: "NAPTR",
+    47: "NSEC",
+    50: "NSEC3",
+    51: "NSEC3PARAM",
+    61: "OPENPGPKEY",
+    46: "RRSIG",
+    17: "RP",
+    24: "SIG",
+    33: "SRV",
+    44: "SSHFP",
+    32768: "TA",
+    249: "TKEY",
+    52: "TLSA",
+    250: "TSIG",
+    256: "URI",
+    41: "OPT",
+    255: "*"
+  ];
+  RecordTypes = assumeUnique(t);
+}
+
