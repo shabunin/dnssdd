ff8f36234bd17cc423bce8ed2fedde98014362ad
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2020-05-27 18:30:05 +0300

    service scanner as an example

--- source/app.d
+++ source/app.d
@@ -1,7 +1,5 @@
-import std.base64;
 import std.bitmanip;
 import std.conv;
-import std.digest;
 import std.stdio;
 import std.socket;
 
@@ -50,9 +48,6 @@ class DnsSD {
     auto receivedLen = sock.receive(buf);
     if(receivedLen > 0) {
       buf.length = receivedLen;
-      writeln("##### ##### #### #### ##### #### #####");
-      writeln("Received: ", buf.toHexString());
-      writeln("##### ##### #### #### ##### #### #####");
 
       // parse
       result = parseRR(buf);
@@ -60,58 +55,86 @@ class DnsSD {
 
     return result;
   }
+
+  public void scanService(string service) {
+    Record query;
+    RecordResponse[string] ptrs;
+    query.header.questions = 1;
+    query.header.response = false;
+    query.questions.length = 1;
+    query.questions[0].label = service;
+    query.questions[0].record_type = RecordTypes.ptr;
+    query.questions[0].record_class = RecordClasses.int_;
+    sendRecord(query);
+    while(true) {
+      Thread.sleep(1.msecs);
+      Record msg = processMessages();
+      if (msg.valid) {
+        if (!msg.header.response) continue;
+        //writeln(msg);
+        for (auto i = 0; i < msg.answers.length; i += 1) {
+          auto ans = msg.answers[i];
+          string label = ans.label;
+          if (ans.record_type == RecordTypes.ptr) {
+            string domain = ans.rdata.data;
+            if ((domain in ptrs) !is null) continue;
+            ptrs[domain] = ans;
+            Thread.sleep(300.msecs);
+            writeln(domain);
+            query.questions[0].label = domain;
+            sendRecord(query);
+          }
+        }
+      }
+    }
+  }
 }
 
 void main() {
   writeln("hello, friend\n");
 
   auto resolver = new DnsSD();
+  resolver.scanService("_services._dns-sd._udp.local");
 
-  Record query;
-  query.header.questions = 1;
-  query.header.response = false;
-  query.questions.length = 1;
-  query.questions[0].label = "_services._dns-sd._udp.local";
-  query.questions[0].record_type = RecordTypes.ptr;
-  query.questions[0].record_class = RecordClasses.int_;
-  resolver.sendRecord(query);
-  Record resp;
-  resp.header.answers = 1;
-  resp.header.additionals = 3;
-  resp.header.response = true;
-  resp.header.authoritative = true;
-  resp.answers.length = 1;
-  resp.answers[0].label = "_batya._tcp.local";
-  resp.answers[0].record_type = RecordTypes.ptr;
-  resp.answers[0].record_class = RecordClasses.int_;
-  resp.answers[0].label = "_batya._tcp.local";
-  resp.answers[0].record_type = RecordTypes.ptr;
-  resp.answers[0].record_class = RecordClasses.int_;
-  resp.answers[0].ttl = 120;
-  resp.answers[0].rdata.data = "_batya._tcp.local";
-  resp.additionals.length = 3;
-  resp.additionals[0].label = "_batya._tcp.local";
-  resp.additionals[0].record_type = RecordTypes.a;
-  resp.additionals[0].record_class = RecordClasses.int_;
-  resp.additionals[0].ttl = 120;
-  resp.additionals[0].rdata.data = "192.168.1.63";
-  resp.additionals[1].label = "_batya._tcp.local";
-  resp.additionals[1].record_type = RecordTypes.srv;
-  resp.additionals[1].record_class = RecordClasses.int_;
-  resp.additionals[1].ttl = 120;
-  resp.additionals[1].rdata.port = 80;
-  resp.additionals[2].label = "_batya._tcp.local";
-  resp.additionals[2].record_type = RecordTypes.txt;
-  resp.additionals[2].record_class = RecordClasses.int_;
-  resp.additionals[2].ttl = 120;
-  resp.additionals[2].rdata.data = "batya=big boldhead\njunior=small boldhead";
-  resolver.sendRecord(resp);
-
+  /**
+    Record resp;
+    resp.header.answers = 1;
+    resp.header.additionals = 3;
+    resp.header.response = true;
+    resp.header.authoritative = true;
+    resp.answers.length = 1;
+    resp.answers[0].label = "_batya._tcp.local";
+    resp.answers[0].record_type = RecordTypes.ptr;
+    resp.answers[0].record_class = RecordClasses.int_;
+    resp.answers[0].label = "_batya._tcp.local";
+    resp.answers[0].record_type = RecordTypes.ptr;
+    resp.answers[0].record_class = RecordClasses.int_;
+    resp.answers[0].ttl = 120;
+    resp.answers[0].rdata.data = "_batya._tcp.local";
+    resp.additionals.length = 3;
+    resp.additionals[0].label = "_batya._tcp.local";
+    resp.additionals[0].record_type = RecordTypes.a;
+    resp.additionals[0].record_class = RecordClasses.int_;
+    resp.additionals[0].ttl = 120;
+    resp.additionals[0].rdata.data = "192.168.1.63";
+    resp.additionals[1].label = "_batya._tcp.local";
+    resp.additionals[1].record_type = RecordTypes.srv;
+    resp.additionals[1].record_class = RecordClasses.int_;
+    resp.additionals[1].ttl = 120;
+    resp.additionals[1].rdata.port = 80;
+    resp.additionals[2].label = "_batya._tcp.local";
+    resp.additionals[2].record_type = RecordTypes.txt;
+    resp.additionals[2].record_class = RecordClasses.int_;
+    resp.additionals[2].ttl = 120;
+    resp.additionals[2].rdata.data = "batya=big boldhead\njunior=small boldhead";
+    resolver.sendRecord(resp);
+   **/
 
   while(true) {
     try {
-      auto rec = resolver.processMessages();
-      if (rec.valid) {
+      /**
+        auto rec = resolver.processMessages();
+        if (rec.valid) {
         writeln(" ===== record parsed ===== ");
         writeln(rec);
         writeln(" ===== serialize:    ===== ");
@@ -121,11 +144,12 @@ void main() {
         writeln("##### ##### #### #### ##### #### #####");
         auto r2 = parseRR(rrs);
         if (r2.valid) {
-          writeln(" ===== serialized parsed ===== ");
+        writeln(" ===== serialized parsed ===== ");
         } else {
-          writeln(" ===== serialized NOT valid ===");
+        writeln(" ===== serialized NOT valid ===");
         }
-      }
+        }
+       **/
       Thread.sleep(1.msecs);
     } catch (Exception e) {
       writeln("Exeption parsing message: ");
